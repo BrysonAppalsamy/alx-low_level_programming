d5af13677abc3afcba58e4f0d0453af367b5b263
BrysonAppalsamy <brysonappalsamy@gmail.com>
2023-02-20 11:33:26 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+#include <ctype.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - program that prints the alphabet in lowercase, followed by a new line
+ * You can only use the putchar
+ * Return: 0
  */
 
 int main(void)
 {
-	char c;
-
-	c = 'a';
-	while (c <= 'z')
-	{
-		puntchar(c);
-	}
-	putchar("\n");
-	return (0);
+int l = 'a';
+while (l <= 'z')
+{
+putchar(l);
+l += 1;
 }
+putchar('\n');
+return (0);
+}
+
